50ad6cd1aed1b6aa1f1f60d93e50686e9bece531
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-05-08 10:27:19 +0300

    install hero icons

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import {
+    ChatIcon,
+    BellIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+
+} from '@heroicons/react/solid';
+
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon
+} from '@heroicons/react/outline'
 
 function Header() {
   return (
     <div>Header
         {/* left */}
+        <div className='flex items-center'>
         <Image
-        source='https://links.papareact.com/5me'
+        src='https://links.papareact.com/5me'
         layout='fixed'
-        height='40'
-        
+        height={40}
+        width={40}
         />
+        
+        <div className='flex ml-2 rounded-full p-2'>
+        <SearchIcon className='h-6'/>
+        <input type='text' placeholder='search'/>
+        </div>
+        </div>
         {/* center */}
         {/* right  */}
     </div>
