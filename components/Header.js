ddd31c4eb7d11d31b9a95155b1a0b8041e6a3099
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-05-08 10:03:41 +0300

    instal tailwind css

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>Header
+        {/* left */}
+        <Image
+        source='https://links.papareact.com/5me'
+        layout='fixed'
+        height='40'
+        
+        />
+        {/* center */}
+        {/* right  */}
+    </div>
+  )
+}
+
+export default Header
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    domains:[
+      "links.papareact.com",
+      "platform-lookside.fbsbx..com",
+      "firebasestorage.googleapis.com"
+    ]
+  }
 }
-
-module.exports = nextConfig
